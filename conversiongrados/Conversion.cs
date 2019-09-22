4aebe5c7eaa45654e9435bff471347b2c02941a1
Kevin Arias <kevinlh1337@gmail.com>
2019-09-22 12:18:01 -0500

    Se agrega el proyecto a github/rama remota.

---
+++ conversiongrados/Conversion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversiongrados
+{
+    public class Conversion
+    {
+        private double grados;
+
+        public void setGrados(double valor)
+        {
+            this.grados = valor;
+        }
+
+        public double getGrados()
+        {
+            return this.grados;
+        }
+
+        public double convertirFarenheit()
+        {
+            return (this.getGrados()*9/5+32);
+        }
+
+        public double convertirCelcius()
+        {
+            return (this.getGrados()-32)*5/9;
+        }
+    }
+}
